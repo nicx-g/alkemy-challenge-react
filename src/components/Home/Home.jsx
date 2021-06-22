f1cbb5f4ab011ba995586047c10e606d3fd18506
Nicolás Gomez <nicx_g@hotmail.com>
2021-06-22 00:56:26 -0300

    Hice el maquetado responsive del home con datos falsos para testear el diseño

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,31 @@
-import {useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import useUser from '../../hooks/useUser';
+import AsideMenu from '../Global/AsideMenu/AsideMenu';
+import SearchBar from '../Global/SearchBar/SearchBar';
+import MyTeam from './MyTeam/MyTeam';
 
 const Home = () => {
-    const {isLogged} = useUser();
+    const { isLogged } = useUser();
     const history = useHistory();
     useEffect(() => {
-        if(!isLogged) history.push('/login')
+        if (!isLogged) history.push('/login')
     }, [isLogged])
-    
+
     return (
-        <div>hola</div>
+        <div className='min-vh-100 row m-0'>
+            <div className='bg-light col-sm-3 p-0 m-0'>
+                <AsideMenu />
+            </div>
+            <div className='bg-white col-sm-9'>
+                <div className="row">
+                    <div className='col-12'>
+                        <SearchBar />
+                        <MyTeam/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
